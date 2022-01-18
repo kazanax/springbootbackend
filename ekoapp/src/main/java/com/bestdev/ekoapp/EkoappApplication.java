570112b9497765503c9b0c131e9b4eb72fc05ba5
kazanax <perezetka@protonmail.com>
2022-01-18 20:58:10 +0100

    Working read only requests

---
+++ ekoapp/src/main/java/com/bestdev/ekoapp/EkoappApplication.java
@@ -0,0 +1,14 @@
+package com.bestdev.ekoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EkoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkoappApplication.class, args);
+	}
+
+}
---
+++ ekoapp/src/test/java/com/bestdev/ekoapp/EkoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestdev.ekoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
